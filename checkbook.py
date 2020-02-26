c829711a58975d371185ec935f5bdf9cc4d44292
Ryan McCall <ryan.e.mccall@gmail.com>
2020-02-25 18:52:36 -0600

    Add verification for user_choice

--- checkbook.py
+++ checkbook.py
@@ -1,10 +1,10 @@
 print()
-print("~~~ Welcome to the Terminal Checkbook App! ~~~")
+print("🌮🌮🌮 Welcome to the Terminal Assisted Checkbook Organizer! 🌮🌮🌮")
 print()
 
-dont_exit = True
+open = True
 
-while dont_exit:
+while open:
     print("How can I help you?")
     print()
     print("1) View Current Balance")
@@ -13,7 +13,27 @@ while dont_exit:
     print("4) Exit")
     print()
     
-    user_choice = input("Your Choice? ")
+    
 
-    if user_choice == "4":
-        dont_exit = False
+    while True:
+        user_choice = input("Your Choice? ")
+
+        if not user_choice.isdigit():
+            print("Sorry, your choice must be 1, 2, 3, or 4.")
+            print()
+            continue
+
+        user_choice = int(user_choice)
+
+        if user_choice > 4 or user_choice < 1:
+            print("Sorry, your choice must be 1, 2, 3, or 4")
+            print()
+            continue
+        else:
+            break
+
+
+    if user_choice == 4:
+        print()
+        print("🌮🌮🌮 Thank you for using T.A.C.O.! 🌮🌮🌮")
+        open = False
